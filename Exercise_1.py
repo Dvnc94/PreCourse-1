2f08903c1baa34c150642d930d68bad0120bf389
Divyanshi Srivastava <divyanshisrivastava@Divyanshis-MBP.attlocal.net>
2025-03-13 16:40:07 -0700

    python

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,35 @@
+'''
+// Time Complexity : O(1)
+// Space Complexity : O(n)
+'''
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
-     def __init__(self):
-         
+      def __init__(self):
+         self.stack=[]
      def isEmpty(self):
-         
+         return len(self.stack)==0
+     
      def push(self, item):
-         
+         self.stack.append(item) 
+
      def pop(self):
-        
+        if self.isEmpty(): 
+            return "Empty" 
+        return self.stack.pop()
         
      def peek(self):
+         if self.isEmpty():
+             return "Empty" 
+         return self.stack[-1] 
         
      def size(self):
+         return len(self.stack) 
          
      def show(self):
+         if self.isEmpty():
+             return "empty"
+         return self.stack
          
 
 s = myStack()
